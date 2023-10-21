77b210a69cefb24abad108da027766fec72fda03
LordPrinz <plazinski2902@gmail.com>
2023-10-21 21:51:09 +0200

    Create tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			gridTemplateColumns: {
+				defaultLayout:
+					"[full-start] minmax(60px,1fr) [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(60px,1fr) [full-end]",
+				smallLayout:
+					"[full-start] minmax(30px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(30px,1fr) [full-end]",
+				mobileLayout:
+					"[full-start] minmax(15px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(15px,1fr) [full-end]",
+			},
 			colors: {
 				"steel-blue": {
 					50: "#f4f9fb",
