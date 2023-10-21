28d4d63a9401f2709b9825666e7f3d3fc4cba2ec
LordPrinz <plazinski2902@gmail.com>
2023-10-21 21:29:22 +0200

    Create navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,44 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
 import HomePage from "./layout/HomePage";
 
+// TODO: Implement loader() for routing
+
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
-		children: [{ index: true, element: <HomePage /> }],
+		children: [
+			{ index: true, element: <HomePage /> },
+			{
+				path: "courses",
+				element: <div>Course list</div>,
+			},
+			{
+				path: "courses/:courseId",
+				element: <div>CourseId</div>,
+			},
+			{
+				path: "courses/:courseId/edit",
+				element: <div>Edit course</div>,
+			},
+			{
+				path: "courses/new",
+				element: <div>Create cours</div>,
+			},
+		],
+	},
+	{
+		path: "/:courseId",
+		element: <div>Learning methods list</div>,
+	},
+
+	{
+		path: "/:courseId/flashcards",
+		element: <div>Flashcards</div>,
+	},
+	{
+		path: "/:courseId/test",
+		element: <div>Test</div>,
 	},
 ]);
 
