14a8b329937cd395f9e14caf45a6a6e4a6203008
LordPrinz <plazinski2902@gmail.com>
2023-10-21 22:19:23 +0200

    Position nav

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/Navigation";
 const RootLayout = () => {
 	return (
-		<>
-			<div>RootLayout</div>
-
+		<div className="rootLayout">
+			<MainNavigation />
 			<Outlet />
-		</>
+		</div>
 	);
 };
 
