d56cd70ccd82e8f2c65b9ae0884ccc6deb60fead
LordPrinz <plazinski2902@gmail.com>
2023-10-22 17:43:07 +0200

    Style grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ export default {
 		extend: {
 			gridTemplateColumns: {
 				defaultLayout:
-					"[full-start] minmax(60px,1fr) [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(60px,1fr) [full-end]",
+					"[full-start] minmax(50px,1fr) [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(50px,1fr) [full-end]",
 				smallLayout:
-					"[full-start] minmax(30px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(30px,1fr) [full-end]",
+					"[full-start] minmax(30px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(30px,1fr) [full-end]",
 				mobileLayout:
-					"[full-start] minmax(15px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,140px) [col-end]) [center-end] minmax(15px,1fr) [full-end]",
+					"[full-start] minmax(15px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(15px,1fr) [full-end]",
 			},
 			colors: {
 				"steel-blue": {
