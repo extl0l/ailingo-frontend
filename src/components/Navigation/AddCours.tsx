f86183da9a7fc808ab0de3da469e6a9f43049b2e
LordPrinz <plazinski2902@gmail.com>
2023-10-22 22:15:39 +0200

    Update todos

--- src/components/Navigation/AddCours.tsx
+++ src/components/Navigation/AddCours.tsx
@@ -2,7 +2,7 @@ import { PlusCircleIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 const AddCours = () => {
 	return (
-		<Link to={"/courses/new"} className="relative w-10">
+		<Link to={"/courses/new"} className="relative w-11">
 			<PlusCircleIcon className="w-full fill-theme-blue-tertiary z-10 absolute  left-0 top-0 -translate-y-1/2" />
 			<div className="bg-theme-blue-light w-7 h-7 rounded-full absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-0"></div>
 		</Link>
