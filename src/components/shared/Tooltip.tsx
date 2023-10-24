7a9a613d8aeaa1c19235d059e93ce3772c20e43e
LordPrinz <plazinski2902@gmail.com>
2023-10-24 21:51:50 +0200

    Install radixui tooltip

---
+++ src/components/shared/Tooltip.tsx
@@ -0,0 +1,12 @@
+type Props = {
+	sideOffset?: number;
+	className?: string;
+	content: string;
+	children: React.ReactNode;
+};
+
+const Tooltip = ({}: Props) => {
+	return <div>Tooltip</div>;
+};
+
+export default Tooltip;
