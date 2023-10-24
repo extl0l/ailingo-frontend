3124b82ab9a266b48e598db6b8eecd18c4a84c39
LordPrinz <plazinski2902@gmail.com>
2023-10-24 22:25:38 +0200

    Fix Button font bug

--- src/components/Games/Flashcards/FlashcardOptions.tsx
+++ src/components/Games/Flashcards/FlashcardOptions.tsx
@@ -1,15 +1,19 @@
+import Button from "../../shared/Button";
+
 type Props = {
-  nextFlashcard: () => void;
-  previousFlashcard: () => void;
+	nextFlashcard: () => void;
+	previousFlashcard: () => void;
 };
 
 const FlashcardOptions = ({ nextFlashcard, previousFlashcard }: Props) => {
-  return (
-    <div className="text-theme-font-light flex items-center w-full justify-center gap-8 my-5">
-      <button onClick={previousFlashcard}>pre</button>
-      <button onClick={nextFlashcard}>next</button>
-    </div>
-  );
+	return (
+		<div className="text-theme-font-light flex items-center w-full justify-center gap-8 my-5">
+			<Button onClick={previousFlashcard} className="">
+				pre
+			</Button>
+			<button onClick={nextFlashcard}>next</button>
+		</div>
+	);
 };
 
 export default FlashcardOptions;
