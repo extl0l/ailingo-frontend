ea97ea9c6d91aeeac73117c097a85e4f4e12d1cd
LordPrinz <plazinski2902@gmail.com>
2023-10-25 21:02:43 +0200

    Create input class

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -3,21 +3,20 @@ import { cn } from "../../utils/tailwind";
 import { forwardRef, InputHTMLAttributes } from "react";
 
 export const inputVariants = cva({
-    variants:{},
-    defaultVariants:{}
+	variants: {},
+	defaultVariants: {},
 });
 
 export interface InputProps
 	extends InputHTMLAttributes<HTMLButtonElement>,
 		VariantProps<typeof inputVariants> {}
 
-const Input = ()=>{
-    return <>
-        <input type="text"/>
-    </>
-}
+const Input = ({ className }: InputProps) => {
+	return (
+		<input type="text" className={cn("panel outline-none py-1.5", className)} />
+	);
+};
 
 Input.displayName = "Input";
 
-
-export default Input
+export default Input;
---
+++ src/components/shared/SpeachButton.tsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
+
+const SpeachButton = () => {
+	return (
+		<Button>
+			<SpeakerWaveIcon />
+		</Button>
+	);
+};
+
+export default SpeachButton;
