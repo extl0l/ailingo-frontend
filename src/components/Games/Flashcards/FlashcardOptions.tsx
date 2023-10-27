0fa23c060fd49a0b17d3e64a339722f542bf9800
LordPrinz <61083743+LordPrinz@users.noreply.github.com>
2023-10-27 12:08:36 +0000

    Create back arrow

--- src/components/Games/Flashcards/FlashcardOptions.tsx
+++ src/components/Games/Flashcards/FlashcardOptions.tsx
@@ -1,34 +1,52 @@
 import Button from "../../shared/Button";
 import { XMarkIcon, CheckIcon } from "@heroicons/react/24/solid";
 import Tooltip from "../../shared/Tooltip";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 type Props = {
-	nextFlashcard: () => void;
-	previousFlashcard: () => void;
+  nextFlashcard: () => void;
+  previousFlashcard: () => void;
 };
 
 const FlashcardOptions = ({ nextFlashcard, previousFlashcard }: Props) => {
-	return (
-		<div className="flex items-center w-full justify-center gap-16 mt-4 mb-5">
-			<Tooltip content="I don't know">
-				<Button
-					variant="outline"
-					rounded="full"
-					onClick={previousFlashcard}
-					className="p-1.5">
-					<XMarkIcon className="w-8 h-8 fill-theme-red-primary" />
-				</Button>
-			</Tooltip>
-			<Tooltip content="I know">
-				<Button
-					onClick={nextFlashcard}
-					variant="outline"
-					rounded="full"
-					className="p-1.5">
-					<CheckIcon className="w-8 h-8 fill-theme-green-primary" />
-				</Button>
-			</Tooltip>
-		</div>
-	);
+  return (
+    <div className="flex w-full items-center justify-between mt-4 mb-5 max-w-5xl mx-auto">
+      <div>
+        <Tooltip content="Go back">
+          <Button
+            variant="outline"
+            rounded="full"
+            onClick={previousFlashcard}
+            className="p-1.5 border-none"
+          >
+            <ArrowUturnLeftIcon className="w-6 h-6 fill-theme-blue-ghost" />
+          </Button>
+        </Tooltip>
+      </div>
+      <div className="gap-16 flex">
+        <Tooltip content="I don't know">
+          <Button
+            variant="outline"
+            rounded="full"
+            onClick={previousFlashcard}
+            className="p-1.5"
+          >
+            <XMarkIcon className="w-8 h-8 fill-theme-red-primary" />
+          </Button>
+        </Tooltip>
+        <Tooltip content="I know">
+          <Button
+            onClick={nextFlashcard}
+            variant="outline"
+            rounded="full"
+            className="p-1.5"
+          >
+            <CheckIcon className="w-8 h-8 fill-theme-green-primary" />
+          </Button>
+        </Tooltip>
+      </div>
+      <div>xd</div>
+    </div>
+  );
 };
 
 export default FlashcardOptions;
