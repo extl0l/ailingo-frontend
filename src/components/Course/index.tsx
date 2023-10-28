89d216ee6c3f5fc585a7ad32caead487f96ae9ea
Pawel B <pawboc12@onet.pl>
2023-10-28 23:32:54 +0200

    Add flashcard on button click

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -1,12 +1,21 @@
 import Button from "../shared/Button";
 import Input from "../shared/Input";
-import { useState } from "react";
+import Flashcard from "./Flashcard";
+import { FormEvent, useState } from "react";
 
 const CreateCourse = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [phraseLanguage, setPhraseLanguage] = useState("");
     const [definitionsLanguage, setDefinitionsLanguage] = useState("");
+    const [prompts, setPrompts] = useState([<Flashcard key="prompt-0" />]);
+    const [promptIndex, setPromptIndex] = useState(1);
+
+    const handleButtonClick = (event: FormEvent) => {
+        event.preventDefault();
+        setPrompts(prompts => [...prompts, <Flashcard key={`prompt-${promptIndex}`} />])
+        setPromptIndex(promptIndex => promptIndex + 1);
+    }
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, setStateFunction: (newValue: string) => void) => {
         const newValue = e.target.value;
@@ -18,13 +27,17 @@ const CreateCourse = () => {
         <form>
             <div className="create-info flex">
                 <div className="text-white">Create flashcards</div>
-                <Button buttonStyle={"blue"}>Create</Button>
+                <Button buttonStyle={"yellow"}>Create</Button>
             </div>
             <div className="inputs grid">
                 <Input placeholder={"Enter title"} rounded={"large"} onChange={(e) => handleInputChange(e, setTitle)} />
                 <Input placeholder={"Enter description"} rounded={"large"} height="md" onChange={(e) => handleInputChange(e, setDescription)} />
                 <Input placeholder={"Phrase language"} rounded={"large"} onChange={(e) => handleInputChange(e, setPhraseLanguage)} />
                 <Input placeholder={"Definitions language"} rounded={"large"} onChange={(e) => handleInputChange(e, setDefinitionsLanguage)} />
+            </div>
+            <div>
+                {...prompts}
+                <Button buttonStyle={"blue"} onClick={handleButtonClick}>Add flashcard</Button>
             </div>
         </form>
     )
