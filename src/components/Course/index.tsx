13ae785ff9a17898a3642b41da6026726d6cc52a
Pawel B <pawboc12@onet.pl>
2023-10-29 20:27:00 +0100

    Add css to flashcards

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -53,23 +53,33 @@ const CreateCourse = () => {
                 <div className="text-white">Create flashcards</div>
                 <Button buttonStyle={"yellow"} onClick={handleSubmit}>Create</Button>
             </div>
-            <div className="inputs grid">
-                <Input placeholder={"Enter title"} rounded={"large"} onChange={(e) => handleInputChange(e, setTitle)} required />
-                <Input placeholder={"Enter description"} rounded={"large"} height="md" onChange={(e) => handleInputChange(e, setDescription)} required />
-                <Input placeholder={"Phrase language"} rounded={"large"} onChange={(e) => handleInputChange(e, setPhraseLanguage)} required />
-                <Input placeholder={"Definitions language"} rounded={"large"} onChange={(e) => handleInputChange(e, setDefinitionsLanguage)} required />
-                <Button buttonStyle={"blue"} className="w-60" onClick={addPrompt}>
-                    Add flashcard
-                </Button>
-                <div>
+
+            <div className="inputs space-y-5">
+                <div className="space-y-2 w-full place-items-center">
+                    <Input className="w-full" placeholder={"Enter title"} rounded={"large"} onChange={(e) => handleInputChange(e, setTitle)} required />
+                    <div className="grid grid-cols-2 w-full space-x-2 align-middle m-auto">
+                        <Input placeholder={"Enter description"} width="md" rounded={"large"} height="md" onChange={(e) => handleInputChange(e, setDescription)} required />
+                        <div className="grid w-full">
+                            <Input width="full" placeholder={"Phrase language"} rounded={"large"} onChange={(e) => handleInputChange(e, setPhraseLanguage)} required />
+                            <Input width="full" placeholder={"Definitions language"} rounded={"large"} onChange={(e) => handleInputChange(e, setDefinitionsLanguage)} required />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="space-y-2 ">
                     {prompts.map((prompt, index) => (
-                        <div key={index} className="flex align-middle">
-                            <Input placeholder={"Enter phrase"} name="phrase" rounded={"large"} value={prompt.phrase} onChange={event => handleFormChange(index, event)} required />
-                            <Input placeholder={"Enter definition"} name="definition" rounded={"large"} value={prompt.definition} onChange={event => handleFormChange(index, event)} onKeyDown={handleEnterKeyPress} required />
-                            <Button buttonStyle={"transparent"} onClick={(event) => { if (prompts.length > 1) { removePrompt(event, index) } }}><TrashIcon className="w-5 h-5" /></Button>
+                        <div key={index} className="grid align-middle grid-cols-3 h-20 panel">
+                            <Input placeholder={"Enter phrase"} name="phrase" rounded={"large"} border={"white"} value={prompt.phrase} onChange={event => handleFormChange(index, event)} required />
+                            <Input placeholder={"Enter definition"} name="definition" rounded={"large"} border={"white"} value={prompt.definition} onChange={event => handleFormChange(index, event)} onKeyDown={handleEnterKeyPress} required />
+                            <Button buttonStyle={"transparent"} className="w-24 bg-red-500" onClick={(event) => { if (prompts.length > 1) { removePrompt(event, index) } }}><TrashIcon className="w-5 h-5" /></Button>
                         </div>
                     ))}
+                </div>
 
+                <div className="flex space-x-1">
+                    <Button buttonStyle={"blue"} className="w-40" onClick={addPrompt}>
+                        Add flashcard
+                    </Button>
                 </div>
             </div>
         </div>
