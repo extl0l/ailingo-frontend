85fafca7d719d8ab426c98f0579dcced4e7d70a0
Pawel B <pawboc12@onet.pl>
2023-10-29 19:30:56 +0100

    Fix remove button

--- src/components/Course/index.tsx
+++ src/components/Course/index.tsx
@@ -14,7 +14,6 @@ const CreateCourse = () => {
         event.preventDefault();
         const newPrompt = { phrase: '', definition: '' }
         setPrompts([...prompts, newPrompt]);
-
     }
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, setStateFunction: (newValue: string) => void) => {
@@ -31,14 +30,19 @@ const CreateCourse = () => {
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        console.table(prompts)
+        const data = { title, description, phraseLanguage, definitionsLanguage, prompts };
+        console.log(data)
     }
 
     const removePrompt = (event: FormEvent, index: number) => {
         event.preventDefault();
+        console.log(prompts.length);
+        console.log(prompts + "\n---------------");
         const data = [...prompts];
         data.splice(index, 1);
         setPrompts(data);
+        console.log(prompts.length);
+        console.log(prompts + "\n-------------------------------");
     }
 
     const handleEnterKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -55,21 +59,22 @@ const CreateCourse = () => {
                 <Button buttonStyle={"yellow"} onClick={handleSubmit}>Create</Button>
             </div>
             <div className="inputs grid">
-                <Input placeholder={"Enter title"} rounded={"large"} onChange={(e) => handleInputChange(e, setTitle)} />
-                <Input placeholder={"Enter description"} rounded={"large"} height="md" onChange={(e) => handleInputChange(e, setDescription)} />
-                <Input placeholder={"Phrase language"} rounded={"large"} onChange={(e) => handleInputChange(e, setPhraseLanguage)} />
-                <Input placeholder={"Definitions language"} rounded={"large"} onChange={(e) => handleInputChange(e, setDefinitionsLanguage)} />
+                <Input placeholder={"Enter title"} rounded={"large"} onChange={(e) => handleInputChange(e, setTitle)} required />
+                <Input placeholder={"Enter description"} rounded={"large"} height="md" onChange={(e) => handleInputChange(e, setDescription)} required />
+                <Input placeholder={"Phrase language"} rounded={"large"} onChange={(e) => handleInputChange(e, setPhraseLanguage)} required />
+                <Input placeholder={"Definitions language"} rounded={"large"} onChange={(e) => handleInputChange(e, setDefinitionsLanguage)} required />
+                <Button buttonStyle={"blue"} className="w-60" onClick={addPrompt}>
+                    Add flashcard
+                </Button>
                 <div>
                     {prompts.map((prompt, index) => (
                         <div key={index} className="flex align-middle">
-                            <Input placeholder={"Enter phrase"} name="phrase" rounded={"large"} value={prompt.phrase} onChange={event => handleFormChange(index, event)} />
-                            <Input placeholder={"Enter definition"} name="definition" rounded={"large"} value={prompt.definition} onChange={event => handleFormChange(index, event)} onKeyDown={handleEnterKeyPress} />
-                            <Button buttonStyle={"transparent"}><TrashIcon className="w-5 h-5" onClick={(event) => { removePrompt(event, index) }} /></Button>
+                            <Input placeholder={"Enter phrase"} name="phrase" rounded={"large"} value={prompt.phrase} onChange={event => handleFormChange(index, event)} required />
+                            <Input placeholder={"Enter definition"} name="definition" rounded={"large"} value={prompt.definition} onChange={event => handleFormChange(index, event)} onKeyDown={handleEnterKeyPress} required />
+                            <Button buttonStyle={"transparent"} onClick={(event) => { if (prompts.length > 1) { removePrompt(event, index) } }}><TrashIcon className="w-5 h-5" /></Button>
                         </div>
                     ))}
-                    <Button buttonStyle={"blue"} onClick={addPrompt}>
-                        Add flashcard
-                    </Button>
+
                 </div>
             </div>
         </div>
