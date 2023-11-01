4a51336a2b0fd0aba7ce1dc39ca367776167dd6b
Bartosz Bieniek <itsbk20@gmail.com>
2023-11-01 23:17:10 +0100

    Move navigation links to a separate component

--- src/features/navigation/MainNavigation.tsx
+++ src/features/navigation/MainNavigation.tsx
@@ -1,28 +1,11 @@
-import {Link} from 'react-router-dom';
 import {UserDetails} from './components/UserDetails.tsx';
+import {NavigationLinks} from './components/NavigationLinks.tsx';
 
 export const MainNavigation = () => {
   return (
       <header>
         <UserDetails/>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/library">My Library</Link>
-            </li>
-            <li>
-              <Link to="/chats">Chats</Link>
-            </li>
-            <li>
-              <button>
-                <img src="" alt="search"/>
-              </button>
-            </li>
-          </ul>
-        </nav>
+        <NavigationLinks/>
       </header>
   );
 };
---
+++ src/features/navigation/components/NavigationLinks.tsx
@@ -0,0 +1,57 @@
+import IconSearch from '../assets/search.svg';
+import {Link, useLocation} from 'react-router-dom';
+import {cn} from '../../../utils/tailwind.ts';
+
+interface NavigationLink {
+  to: string,
+  title: string,
+}
+
+const defaultNavigationLinks: NavigationLink[] = [
+  {to: '/', title: 'Home'},
+  {to: '/library', title: 'My library'},
+  {to: '/chats', title: 'Chats'}];
+
+export const NavigationLinks = () => {
+  const location = useLocation();
+
+  return (<nav>
+    <ul className="flex gap-3.5 items-center">
+      {defaultNavigationLinks.map(link => (<li key={link.to}>
+        <StyledNavigationLink
+            link={link}
+            isActive={location.pathname === link.to}
+        />
+      </li>))}
+      <li>
+        {/*TODO: Open search box*/}
+        <button
+            className="block opacity-50 hover:opacity-100 transition-opacity">
+          <img src={IconSearch} alt="search"/>
+        </button>
+      </li>
+    </ul>
+  </nav>);
+};
+
+interface StyledNavigationLinkProps {
+  link: NavigationLink;
+  isActive?: boolean;
+}
+
+const StyledNavigationLink = (props: StyledNavigationLinkProps) => {
+  const {link, isActive} = props;
+
+  const className = 'font-medium text-xl text-theme-brown-light opacity-50 hover:opacity-100 transition-opacity relative';
+  return (
+      <Link to={link.to} className={cn(className, isActive && 'opacity-100')}>
+        {link.title}
+        {isActive && <ActiveLinkDecoration/>}
+      </Link>
+  );
+};
+
+const ActiveLinkDecoration = () => {
+  return <span
+      className="w-1.5 h-1.5 block bg-theme-green-light rounded-full absolute top-0 right-0 translate-x-1/2"/>;
+};
