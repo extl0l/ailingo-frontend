0a3664d614e4c21fdd62447cbd8578677ccc10c3
LordPrinz <plazinski2902@gmail.com>
2023-11-04 13:27:20 +0100

    Install react-query

--- src/App.tsx
+++ src/App.tsx
@@ -5,77 +5,81 @@ import CreateCourse from "./components/Course/index";
 import AuthProvider from "./features/auth/AuthProvider.tsx";
 import { HomePage } from "./features/home/HomePage.tsx";
 import { LibraryLayout } from "./features/library/LibraryLayout.tsx";
-import { MySetsPage } from "./features/library/MySetsPage.tsx";
+import {
+	loader as mySetsLoader,
+	MySetsPage,
+} from "./features/library/MySetsPage.tsx";
 import { StarredSetsPage } from "./features/library/StarredSetsPage.tsx";
 import { RecentlyStudiedPage } from "./features/library/RecentlyStudiedPage.tsx";
 
 // TODO: Implement loader() for routing
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "library",
-        element: <LibraryLayout />,
-        children: [
-          {
-            index: true,
-            element: <MySetsPage />,
-          },
-          {
-            path: "starred",
-            element: <StarredSetsPage />,
-          },
-          {
-            path: "recent",
-            element: <RecentlyStudiedPage />,
-          },
-        ],
-      },
-      {
-        path: "courses/:courseId",
-        element: <div>CourseId</div>,
-      },
-      {
-        path: "courses/:courseId/edit",
-        element: <div>Edit course</div>,
-      },
-      {
-        path: "courses/new",
-        element: <CreateCourse />,
-      },
-    ],
-  },
-  {
-    path: "/:courseId",
-    element: <div>Learning methods list</div>,
-  },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: "library",
+				element: <LibraryLayout />,
+				children: [
+					{
+						index: true,
+						element: <MySetsPage />,
+						loader: mySetsLoader,
+					},
+					{
+						path: "starred",
+						element: <StarredSetsPage />,
+					},
+					{
+						path: "recent",
+						element: <RecentlyStudiedPage />,
+					},
+				],
+			},
+			{
+				path: "courses/:courseId",
+				element: <div>CourseId</div>,
+			},
+			{
+				path: "courses/:courseId/edit",
+				element: <div>Edit course</div>,
+			},
+			{
+				path: "courses/new",
+				element: <CreateCourse />,
+			},
+		],
+	},
+	{
+		path: "/:courseId",
+		element: <div>Learning methods list</div>,
+	},
 
-  {
-    path: "/:courseId/flashcards",
-    element: <Flashcards />,
-  },
-  {
-    path: "/:courseId/test",
-    element: <div>Test</div>,
-  },
-  {
-    path: "/login",
-    element: <div>Login</div>,
-  },
+	{
+		path: "/:courseId/flashcards",
+		element: <Flashcards />,
+	},
+	{
+		path: "/:courseId/test",
+		element: <div>Test</div>,
+	},
+	{
+		path: "/login",
+		element: <div>Login</div>,
+	},
 ]);
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+	);
 };
 
 export default App;
--- src/features/library/MySetsPage.tsx
+++ src/features/library/MySetsPage.tsx
@@ -4,50 +4,89 @@ import { StudySetCard } from "../_shared/components/StudySetCard.tsx";
 import { Glyph } from "../_shared/components/Glyph.tsx";
 
 import IconSortDescending from "./assets/arrow_upward_alt_FILL0_wght400_GRAD0_opsz40.svg";
+import { json, useLoaderData } from "react-router";
 
-const mySets = [
-  {
-    id: "sts-00",
-    title: "Gardening",
-    icon: IconFurniture,
-    color: "hsla(159, 39%, 55%, 1)",
-  },
-  {
-    id: "sts-01",
-    title: "At the airport",
-    icon: IconFurniture,
-    color: "hsla(58, 63%, 53%, 1)",
-  },
-  {
-    id: "sts-02",
-    title: "Biking in the suburbs",
-    icon: IconFurniture,
-    color: "hsla(24, 93%, 56%, 1)",
-  },
-  {
-    id: "sts-03",
-    title: "Home furnishings",
-    icon: IconFurniture,
-    color: "hsla(22,22%,27%,1)",
-  },
-];
+// import axios from "axios";
+// import { useAuth } from "@clerk/clerk-react";
+
+export type MySet = {
+	id: string;
+	title: string;
+	icon: string;
+	color: string;
+};
 
 export const MySetsPage = () => {
-  return (
-    <>
-      <div className="col-span-full mt-6 flex justify-between items-end">
-        <h1 className="font-medium text-theme-brown-light text-2xl">
-          Created by me
-        </h1>
-        <button className="flex items-center gap-0.5 font-medium">
-          Recent
-          <Glyph src={IconSortDescending} width="1.5rem" height="1.5rem" />
-        </button>
-      </div>
-      <CreateNewStudySetCard />
-      {mySets.map((studySet) => (
-        <StudySetCard key={studySet.id} studySet={studySet} />
-      ))}
-    </>
-  );
+	const sets = useLoaderData() as MySet[] | null;
+
+	// const auth = useAuth();
+
+	// auth.getToken().then((data) => console.log(data));
+
+	return (
+		<>
+			<div className="col-span-full mt-6 flex justify-between items-end">
+				<h1 className="font-medium text-theme-brown-light text-2xl">
+					Created by me
+				</h1>
+				<button className="flex items-center gap-0.5 font-medium">
+					Recent
+					<Glyph src={IconSortDescending} width="1.5rem" height="1.5rem" />
+				</button>
+			</div>
+			<CreateNewStudySetCard />
+			{sets?.map((studySet) => (
+				<StudySetCard key={studySet.id} studySet={studySet} />
+			))}
+		</>
+	);
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async () => {
+	// console.log(data);
+
+	// const url = `${import.meta.env.VITE_BACKEND_URL}/me/studysets/created`;
+
+	// //TODO: Find token
+
+	// const token = ""
+
+	// const config = {
+	// 	headers: { Authorization: `Bearer ${token}` },
+	// };
+
+	// const data = await axios.get(url, config);
+	// console.log(data);
+
+	//? Testing
+
+	const mySets = [
+		{
+			id: "sts-00",
+			title: "Gardening",
+			icon: IconFurniture,
+			color: "hsla(159, 39%, 55%, 1)",
+		},
+		{
+			id: "sts-01",
+			title: "At the airport",
+			icon: IconFurniture,
+			color: "hsla(58, 63%, 53%, 1)",
+		},
+		{
+			id: "sts-02",
+			title: "Biking in the suburbs",
+			icon: IconFurniture,
+			color: "hsla(24, 93%, 56%, 1)",
+		},
+		{
+			id: "sts-03",
+			title: "Home furnishings",
+			icon: IconFurniture,
+			color: "hsla(22,22%,27%,1)",
+		},
+	];
+
+	return json(mySets);
 };
