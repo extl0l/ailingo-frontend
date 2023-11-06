b9830402edb4481ef1961a4a208fe75520d9cc5a
Bartosz Bieniek <itsbk20@gmail.com>
2023-11-06 23:13:19 +0100

    Implement definition creation

--- src/features/studyset/components/DefinitionList.tsx
+++ src/features/studyset/components/DefinitionList.tsx
@@ -1,5 +1,11 @@
 import { EditableText } from "../../_shared/components/EditableText.tsx";
-import { useCallback, useEffect, useState } from "react";
+import {
+  ChangeEvent,
+  FormEvent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { cn } from "../../../utils/tailwind.ts";
 
 export interface Definition {
@@ -64,6 +70,12 @@ export const DefinitionList = () => {
     setDefinitions(definitionsCopy);
   };
 
+  const handleDefinitionCreate = (definition: Omit<Definition, "id">) => {
+    setDefinitions((definitions) => {
+      return [...definitions, { ...definition, id: Date.now() }];
+    });
+  };
+
   return (
     <section className="flex flex-col max-w-3xl px-4 mx-auto">
       <p className="font-medium text-theme-brown-light text-2xl py-6 px-4">
@@ -80,7 +92,84 @@ export const DefinitionList = () => {
           editable={true} // TODO: This should be variable
         />
       ))}
+      <NewDefinitionListItem onCreate={handleDefinitionCreate} />
     </section>
+  );
+};
+
+interface NewDefinitionListItemProps {
+  onCreate?: (definition: Omit<Definition, "id">) => void;
+}
+
+const NewDefinitionListItem = (props: NewDefinitionListItemProps) => {
+  const [phrase, setPhrase] = useState("");
+  const [meaning, setMeaning] = useState("");
+
+  const handlePhraseChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPhrase(event.target.value);
+  };
+
+  const handleMeaningChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMeaning(event.target.value);
+  };
+
+  const handleDefinitionSubmit = () => {
+    const trimmedPhrase = phrase.trim();
+    const trimmedMeaning = meaning.trim();
+
+    if (trimmedPhrase === "" || trimmedMeaning === "") {
+      return;
+    }
+
+    const createdDefinition: Omit<Definition, "id"> = {
+      phrase: trimmedPhrase,
+      meaning: trimmedMeaning,
+      sentences: [],
+    };
+    props.onCreate?.(createdDefinition);
+    setPhrase("");
+    setMeaning("");
+  };
+
+  const handleDefinitionFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleDefinitionSubmit();
+  };
+
+  const handleFieldBlur = () => {
+    handleDefinitionSubmit();
+  };
+
+  return (
+    <div className="group even:bg-theme-background-light-variant rounded-xl px-6 mx-4">
+      <form className="grid grid-cols-3" onSubmit={handleDefinitionFormSubmit}>
+        <input
+          className="w-full py-3 outline-0 bg-transparent"
+          type="text"
+          value={phrase}
+          onChange={handlePhraseChange}
+          onBlur={handleFieldBlur}
+          placeholder="Type phrase…"
+        />
+        <div className="col-span-2 relative">
+          <input
+            className="w-full py-3 outline-0 bg-transparent"
+            type="text"
+            value={meaning}
+            onChange={handleMeaningChange}
+            onBlur={handleFieldBlur}
+            placeholder="Type definition or…"
+          />
+          <button
+            className="bg-theme-ai-light text-white text-sm py-1 px-5 rounded-full absolute left-[16ch] top-1/2 -translate-y-1/2 disabled:bg-theme-ai-light-variant"
+            disabled={phrase === ""}
+            hidden={meaning !== ""}
+          >
+            Generate with AI
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,87 +3,91 @@
 import tailwindcss3d from "tailwindcss-3d";
 
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			gridTemplateColumns: {
-				defaultLayout:
-					"[full-start] minmax(50px,1fr) [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(50px,1fr) [full-end]",
-				smallLayout:
-					"[full-start] minmax(30px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(30px,1fr) [full-end]",
-				mobileLayout:
-					"[full-start] minmax(15px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(15px,1fr) [full-end]",
-			},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        defaultLayout:
+          "[full-start] minmax(50px,1fr) [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(50px,1fr) [full-end]",
+        smallLayout:
+          "[full-start] minmax(30px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(30px,1fr) [full-end]",
+        mobileLayout:
+          "[full-start] minmax(15px,1fr)  [center-start] repeat(8,[col-start] minmax(min-content,152px) [col-end]) [center-end] minmax(15px,1fr) [full-end]",
+      },
 
-			colors: {
-				"steel-blue": {
-					50: "#f4f9fb",
-					100: "#e9f3f6",
-					200: "#cce4eb",
-					300: "#a0cada",
-					400: "#6ca9c6",
-					500: "#4786ae",
-					600: "#366591",
-					700: "#2d4d76",
-					800: "#273d62",
-					900: "#263354",
-					950: "#192038",
-				},
-				"black-rock": {
-					50: "#eaf1ff",
-					100: "#dae4ff",
-					200: "#bccdff",
-					300: "#93acff",
-					400: "#687dff",
-					500: "#4550ff",
-					600: "#2a25ff",
-					700: "#221ae8",
-					800: "#1c18bb",
-					900: "#1d1d92",
-					950: "#0a092d",
-				},
+      colors: {
+        "steel-blue": {
+          50: "#f4f9fb",
+          100: "#e9f3f6",
+          200: "#cce4eb",
+          300: "#a0cada",
+          400: "#6ca9c6",
+          500: "#4786ae",
+          600: "#366591",
+          700: "#2d4d76",
+          800: "#273d62",
+          900: "#263354",
+          950: "#192038",
+        },
+        "black-rock": {
+          50: "#eaf1ff",
+          100: "#dae4ff",
+          200: "#bccdff",
+          300: "#93acff",
+          400: "#687dff",
+          500: "#4550ff",
+          600: "#2a25ff",
+          700: "#221ae8",
+          800: "#1c18bb",
+          900: "#1d1d92",
+          950: "#0a092d",
+        },
 
-				theme: {
-					font: {
-						light: "#f6f7fb",
-						dark: "#282e3e",
-					},
-					blue: {
-						primary: "#0a092d",
-						secondary: "#2e3856",
-						tertiary: "#4255ff",
-						light: "#edefff",
-						pitch: "#a8b1ff",
-						ghost: "#586380",
-					},
-					yellow: {
-						primary: "#ffcd1f",
-					},
-					red: {
-						primary: "#d05700",
-					},
-					// NEW THEME
-					brown: {
-						light: "hsla(23, 22%, 27%, 1)",
-						lighter: "hsla(23, 22%, 27%, .35)",
-					},
-					green: {
-						primary: "#18ae79", // TODO: Remove after the revamp is finished
-						light: "hsla(159, 39%, 55%, 1)",
-					},
-					lime: {
-						light: "hsla(58, 63%, 53%, 1)",
-					},
-					orange: {
-						light: "hsla(24, 93%, 56%, 1)",
-					},
-					background: {
-						light: "hsla(57,76%,95%,1)",
-						"light-variant": "hsla(58, 73%, 90%, 1)",
-					},
-				},
-			},
-		},
-	},
-	plugins: [tailwindcss3d],
+        theme: {
+          font: {
+            light: "#f6f7fb",
+            dark: "#282e3e",
+          },
+          blue: {
+            primary: "#0a092d",
+            secondary: "#2e3856",
+            tertiary: "#4255ff",
+            light: "#edefff",
+            pitch: "#a8b1ff",
+            ghost: "#586380",
+          },
+          yellow: {
+            primary: "#ffcd1f",
+          },
+          red: {
+            primary: "#d05700",
+          },
+          // NEW THEME
+          ai: {
+            light: "hsl(252,79%,69%)",
+            "light-variant": "hsl(252,10%,86%)",
+          },
+          brown: {
+            light: "hsla(23, 22%, 27%, 1)",
+            lighter: "hsla(23, 22%, 27%, .35)",
+          },
+          green: {
+            primary: "#18ae79", // TODO: Remove after the revamp is finished
+            light: "hsla(159, 39%, 55%, 1)",
+          },
+          lime: {
+            light: "hsla(58, 63%, 53%, 1)",
+          },
+          orange: {
+            light: "hsla(24, 93%, 56%, 1)",
+          },
+          background: {
+            light: "hsla(57,76%,95%,1)",
+            "light-variant": "hsla(58, 73%, 90%, 1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [tailwindcss3d],
 };
