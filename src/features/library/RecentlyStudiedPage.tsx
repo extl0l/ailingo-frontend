6c4e730d7d5705a4059eace1ff8fb596a1cf2816
LordPrinz <plazinski2902@gmail.com>
2023-11-07 23:26:31 +0100

    Create links

--- src/features/library/RecentlyStudiedPage.tsx
+++ src/features/library/RecentlyStudiedPage.tsx
@@ -4,52 +4,43 @@ import { StudySetCard } from "../_shared/components/StudySetCard.tsx";
 import useAuthQuery from "../../hooks/useAuthQuery.ts";
 import { useQuery } from "@tanstack/react-query";
 import { RecentStudySet } from "../_shared/models/RecentStudySet.ts";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const RecentlyStudiedPage = () => {
+	const { queryFn: queryRecentStudy } = useAuthQuery({
+		endpoint: "/me/study-sessions",
+	});
 
-  const { setId } = useParams<{ setId: string }>();
+	const { data } = useQuery({
+		queryKey: ["recent-study-sets"],
+		queryFn: queryRecentStudy,
+	});
 
-  const { queryFn: queryRecentStudy } = useAuthQuery({
-    endpoint: "/me/study-sessions",
-  });
+	const ads = data?.data as RecentStudySet[];
 
-  const { data } = useQuery({
-    queryKey: ["recent-study-sets"],
-    queryFn: queryRecentStudy,
-  })
-
-  const ads = data?.data as RecentStudySet[];
-  console.log(ads);
-
-  return (
-    <>
-      <div className="col-span-full mt-6 flex justify-between items-end">
-        <h1 className="font-medium text-theme-brown-light text-2xl">
-          Recently studied
-        </h1>
-        <button className="flex items-center gap-0.5 font-medium">
-          Study time
-          <Glyph src={IconSortDescending} width="1.5rem" height="1.5rem" />
-        </button>
-      </div>
-      {ads?.map(({ studySet }) => {
-        return (
-          <div>
-            <p>{ }</p>
-            <Link to={`sets/${setId}`} key={studySet.id}>
-              <StudySetCard
-                name={studySet.name}
-                icon={studySet.icon}
-                color={studySet.color}
-
-                authorUsername={studySet.author.username}
-              />
-            </Link>
-          </div>
-        );
-      })}
-
-    </>
-  );
+	return (
+		<>
+			<div className="col-span-full mt-6 flex justify-between items-end">
+				<h1 className="font-medium text-theme-brown-light text-2xl">
+					Recently studied
+				</h1>
+				<button className="flex items-center gap-0.5 font-medium">
+					Study time
+					<Glyph src={IconSortDescending} width="1.5rem" height="1.5rem" />
+				</button>
+			</div>
+			{ads?.map(({ studySet }) => {
+				return (
+					<Link to={`/sets/${studySet.id}`} key={studySet.id}>
+						<StudySetCard
+							name={studySet.name}
+							icon={studySet.icon}
+							color={studySet.color}
+							authorUsername={studySet.author.username}
+						/>
+					</Link>
+				);
+			})}
+		</>
+	);
 };
