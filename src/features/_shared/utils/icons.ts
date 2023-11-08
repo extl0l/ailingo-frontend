80db7a69e9f4c626e54b28a058241c4907fcb10c
Bartosz Bieniek <itsbk20@gmail.com>
2023-11-08 18:07:52 +0100

    Add icons to choose from when editing sets

--- src/features/_shared/utils/icons.ts
+++ src/features/_shared/utils/icons.ts
@@ -1,6 +1,20 @@
 import IconBrokenImage from "../../../assets/icons/broken_image_FILL0_wght400_GRAD0_opsz40.svg";
 
-const ICON_MAP: { [key: string]: string } = {};
+const ICON_MAP: { [key: string]: string } = {
+  chess: "/icons/chess_FILL0_wght400_GRAD0_opsz40.svg",
+  "take-off": "/icons/flight_takeoff_FILL0_wght400_GRAD0_opsz40.svg",
+  luggage: "/icons/luggage_FILL0_wght400_GRAD0_opsz40.svg",
+  clock: "/icons/schedule_FILL0_wght400_GRAD0_opsz40.svg",
+  "game-controller": "/icons/sports_esports_FILL0_wght400_GRAD0_opsz40.svg",
+  volleyball: "/icons/sports_volleyball_FILL0_wght400_GRAD0_opsz40.svg",
+  tactic: "/icons/tactic_FILL0_wght400_GRAD0_opsz40.svg",
+  "beach-accessories": "/icons/beach_access_FILL0_wght400_GRAD0_opsz40.svg",
+  forest: "/icons/forest_FILL0_wght400_GRAD0_opsz40.svg",
+  raven: "/icons/raven_FILL0_wght400_GRAD0_opsz40.svg",
+  dog: "/icons/sound_detection_dog_barking_FILL0_wght400_GRAD0_opsz40.svg",
+  sun: "/icons/sunny_FILL0_wght400_GRAD0_opsz40.svg",
+  water: "/icons/water_drop_FILL0_wght400_GRAD0_opsz40.svg",
+};
 
 export const availableIcons = Object.keys(ICON_MAP);
 
