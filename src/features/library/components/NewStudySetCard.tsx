e4ffc7fa7e9446aab482121024a5bbecb5e0831e
Bartosz Bieniek <itsbk20@gmail.com>
2023-11-08 01:45:03 +0100

    Fix padding on create new study set button Fix color picker not showing the right color for the newly created set

--- src/features/library/components/NewStudySetCard.tsx
+++ src/features/library/components/NewStudySetCard.tsx
@@ -4,37 +4,38 @@ import { cn } from "../../../utils/tailwind.ts";
 import { useClerk, useUser } from "@clerk/clerk-react";
 
 interface NewStudySetCardProps {
-	onClick?: () => void;
-	disabled?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
 export const CreateNewStudySetCard = (props: NewStudySetCardProps) => {
-	const clerk = useClerk();
-	const { isSignedIn } = useUser();
+  const clerk = useClerk();
+  const { isSignedIn } = useUser();
 
-	const handleClick = () => {
-		if (!isSignedIn) {
-			clerk.openSignIn();
-			return;
-		}
-		props.onClick?.();
-	};
+  const handleClick = () => {
+    if (!isSignedIn) {
+      clerk.openSignIn();
+      return;
+    }
+    props.onClick?.();
+  };
 
-	return (
-		<article
-			className={cn(
-				"font-medium text-theme-brown-light p-3 rounded-xl mx-auto px-8 ",
-				props.disabled
-					? "cursor-wait bg-theme-background-light-darker"
-					: "bg-theme-background-light-variant hover:bg-theme-background-light-darker cursor-pointer w-full "
-			)}
-			onClick={handleClick}>
-			<div className="flex items-center gap-2.5">
-				<div className="w-20 h-20 rounded-xl flex items-center justify-center text-theme-lime-light border-[3px] border-theme-lime-light border-dashed">
-					<Glyph src={IconNew} />
-				</div>
-				<p className="text-xl">Create new…</p>
-			</div>
-		</article>
-	);
+  return (
+    <article
+      className={cn(
+        "font-medium text-theme-brown-light p-3 rounded-xl mx-auto",
+        props.disabled
+          ? "cursor-wait bg-theme-background-light-darker"
+          : "bg-theme-background-light-variant hover:bg-theme-background-light-darker cursor-pointer w-full ",
+      )}
+      onClick={handleClick}
+    >
+      <div className="flex items-center gap-2.5">
+        <div className="w-20 h-20 rounded-xl flex items-center justify-center text-theme-lime-light border-[3px] border-theme-lime-light border-dashed">
+          <Glyph src={IconNew} />
+        </div>
+        <p className="text-xl">Create new…</p>
+      </div>
+    </article>
+  );
 };
